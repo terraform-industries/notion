4c1929b4312f84be52b4a97810e6ef6e8ba344bb
tobiascornille <cornilletobias@gmail.com>
2025-07-03 17:59:29 +0200

    Make phone number optional

--- src/models/properties.rs
+++ src/models/properties.rs
@@ -440,7 +440,7 @@ pub enum PropertyValue {
     /// <https://developers.notion.com/reference/property-object#phone-number-configuration>
     PhoneNumber {
         id: PropertyId,
-        phone_number: String,
+        phone_number: Option<String>,
     },
     /// <https://developers.notion.com/reference/property-object#created-time-configuration>
     CreatedTime {
@@ -527,7 +527,7 @@ pub enum RollupPropertyValue {
         email: Option<String>,
     },
     PhoneNumber {
-        phone_number: String,
+        phone_number: Option<String>,
     },
     CreatedTime {
         created_time: DateTime<Utc>,
